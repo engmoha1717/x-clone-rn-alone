aaa43622ca3bd2e3849f4c0eb547ed85381e35fe
engmoha1717 <engmoha1717@gmail.com>
2025-11-09 23:41:53 -0800

    api url

--- front/components/PostComposer.tsx
+++ front/components/PostComposer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const PostComposer = () => {
   return (
     <View className="border-b border-gray-100 p-4 bg-white">
-      <View className="flex-row">
+      {/* <View className="flex-row">
         <Image source={{ uri: user?.imageUrl }} className="w-12 h-12 rounded-full mr-3" />
         <View className="flex-1">
           <TextInput
@@ -17,7 +17,7 @@ const PostComposer = () => {
             maxLength={280}
           />
         </View>
-      </View>
+      </View> */}
 
       <Text>PostComposer</Text>
     </View>
