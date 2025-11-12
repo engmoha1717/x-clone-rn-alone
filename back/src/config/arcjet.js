e38a696eaf4ccaf07ef9615fb27bc2acaa56e8a6
engmoha1717 <engmoha1717@gmail.com>
2025-11-12 10:04:29 -0800

    sa

--- back/src/config/arcjet.js
+++ back/src/config/arcjet.js
@@ -28,36 +28,3 @@ export const aj = arcjet({
     }),
   ],
 });
-
-//   const decision = await aj.protect(req);
-
-//   console.log("Arcjet decision:", decision);
-
-//   if (decision.isDenied()) {
-//     if (decision.reason.isBot()) {
-//       res.writeHead(403, { "Content-Type": "text/html" });
-//       res.end("<h1>Forbidden</h1><p>Bots denied.</p>");
-//     } else if (decision.reason.isRateLimit()) {
-//       res.writeHead(429, { "Content-Type": "text/html" });
-//       res.end("<h1>Too Many Requests</h1><p>Rate limit exceeded.</p>");
-//     } else {
-//       res.writeHead(403, { "Content-Type": "text/html" });
-//       res.end("<h1>Forbidden</h1><p>Access denied.</p>");
-//     }
-//   }
-//   // https://docs.arcjet.com/bot-protection/reference#bot-verification
-//   // Test it with:
-//   // `curl -H "User-Agent: Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" http://localhost:3000`
-//   else if (decision.results.some(isSpoofedBot)) {
-//     res.writeHead(403, { "Content-Type": "text/html" });
-//     res.end("<h1>Forbidden</h1><p>No spoofing!</p>");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use(express.static(path.join(__dirname, "../public")));
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
